c339d7d83df650849a72015c03eb54cb4567c6dc
svitlana mykolaienko <svetlana.mikolaenko@gmail.com>
2022-11-01 20:00:30 +0200

    test

--- src/main/java/com/salesforce/framework/config/TestConfig.java
+++ src/main/java/com/salesforce/framework/config/TestConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
 
 @Config.Sources("classpath:config.properties")
-public interface TestConfig extends Config {
+public interface TestConfig extends Config{
     TestConfig CONFIG = ConfigFactory.create(TestConfig.class, System.getenv(), System.getProperties());
 
     @Key("browser")
